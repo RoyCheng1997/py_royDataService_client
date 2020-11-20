6a54e05a4e65b43b1ec0004d03474661d8ea827f
CoachmanFund <roycheng1997@gmail.com>
2020-11-19 23:58:12 -0500

    add

--- dataApi.py
+++ dataApi.py
@@ -13,7 +13,7 @@ nan, NaN = np.nan, np.nan
 
 def getFuturesData(futures, start, end, freq, datatype, token, adjustS=False):
     '''Get Futures Data'''
-    print("Getting Data...")
+    print("Getting Futures Data...")
     payload = {'futures': futures, 'start' : start, 'end' : end, 'freq' : freq, 'datatype' : datatype, 'adjustS':adjustS, 'token' : token}
     res = requests.get(PATH+"/futures", params=payload)
     if("ERROR" in res.text):
@@ -24,3 +24,18 @@ def getFuturesData(futures, start, end, freq, datatype, token, adjustS=False):
     except:
         pass
     return df
+
+
+def getFuturesIndiData(futures, start, end, freq, datatype, token, adjustS=False, field='close_price', indicator='MA', indi_dict={'timeperiod': 14, 'matype': 1}):
+    '''Get Futures Indicator Data'''
+    print("Getting Futures Indicator Data...")
+    payload = locals()
+    res = requests.get(PATH+"/futures_indi", params=payload)
+    if("ERROR" in res.text):
+        return pd.DataFrame({"ERROR:" : res.text})
+    seri = pd.Series(eval(res.text))
+    try:
+        seri.index = pd.to_datetime(seri.index)
+    except:
+        pass
+    return seri
