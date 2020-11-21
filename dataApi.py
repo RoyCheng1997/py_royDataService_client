d04f201bf96384ae4eb5a1cd8556b87b623ca332
CoachmanFund <roycheng1997@gmail.com>
2020-11-20 21:17:55 -0500

    update

--- dataApi.py
+++ dataApi.py
@@ -12,7 +12,19 @@ PATH = 	"http://roycheng.natapp1.cc"
 nan, NaN = np.nan, np.nan
 
 def getFuturesData(futures, start, end, freq, datatype, token, adjustS=False):
-    '''Get Futures Data'''
+    '''
+    Get Futures Data
+    
+    Args*:
+        futures: str, futures ticker, active contract X000
+        start: str, can be turned to pd.datetime
+        end: str, can be turned to pd.datetime
+        freq: str, requesting frequency
+        datatype: str, data type, see more in github page
+        token: str, the token you have
+        adjustS: boolean, whether to adjust the data, work for active contract
+    
+    '''
     print("Getting Futures Data...")
     payload = {'futures': futures, 'start' : start, 'end' : end, 'freq' : freq, 'datatype' : datatype, 'adjustS':adjustS, 'token' : token}
     res = requests.get(PATH+"/futures", params=payload)
@@ -27,8 +39,23 @@ def getFuturesData(futures, start, end, freq, datatype, token, adjustS=False):
 
 
 def getFuturesIndiData(futures, start, end, freq, datatype, token, adjustS=False, field='close_price', indicator='MA', indi_dict={'timeperiod': 14, 'matype': 1}):
-    '''Get Futures Indicator Data'''
+    '''
+    Get Futures Indicator Data
+    
+    Args*:
+        futures: str, futures ticker, active contract X000
+        start: str, can be turned to pd.datetime
+        end: str, can be turned to pd.datetime
+        freq: str, requesting frequency
+        datatype: str, data type, see more in github page
+        token: str, the token you have
+        adjustS: boolean, whether to adjust the data, work for active contract
+        field: str, field to use in priceshot
+        indicator: str, indicator name, ask author for more indicators and paraDict
+        indi_dict: dict, used to calculate indicator, ask author for more indicators and paraDict
+    '''
     print("Getting Futures Indicator Data...")
+    indi_dict = str(indi_dict)
     payload = locals()
     res = requests.get(PATH+"/futures_indi", params=payload)
     if("ERROR" in res.text):
@@ -39,3 +66,36 @@ def getFuturesIndiData(futures, start, end, freq, datatype, token, adjustS=False
     except:
         pass
     return seri
+
+
+def getFuturesIndiData1(string, start, end, token):
+    '''
+    Get Futures Indicator Data, faster version
+    String format: "M000 MA @timeperiod:20|matype:1"
+    
+    Args*:
+        string: str, request string, see above format, "ticker indicatorName @parameter1:value1|parameter2:value2"
+        start: str, can be turned to pd.datetime
+        end: str, can be turned to pd.datetime
+        token: str, the token you have
+    '''
+    print("Getting Futures Indicator Data Fast...")
+    payload = locals()
+    res = requests.get(PATH+"/futures_indi1", params=payload)
+    if("ERROR" in res.text):
+        return pd.DataFrame({"ERROR:" : res.text})
+    seri = pd.Series(eval(res.text))
+    try:
+        seri.index = pd.to_datetime(seri.index)
+    except:
+        pass
+    return seri
+
+    
+    
+    
+    
+    
+    
+    
+    
--- runDemo.py
+++ runDemo.py
@@ -7,5 +7,16 @@ import dataApi
 
 TOKEN = 'T3ZRzqjcPVxO2P9MEhFvEZRHSJ6lXjqpu6WoknKEZsHqAU26Bv'
 
+# Ordinary request
 df = dataApi.getFuturesData('M000', '2019/1/11', '2019/5/11', '1_d', datatype='tradingData',adjustS=True,token=TOKEN)
 print(df.head())
+
+# Request for futures indicators
+seri = dataApi.getFuturesIndiData('M000', '2019/1/11', '2019/5/11', '1_d', datatype='tradingData',adjustS=True, token=TOKEN,
+                              field='close_price', indicator='MA', indi_dict={'timeperiod': 20, 'matype': 1})
+
+# Fast request for futures indicators
+seri = dataApi.getFuturesIndiData1("M1905 MA @timeperiod:20|matype:1", '2019/1/11', '2019/5/11', token=TOKEN)
+print(seri.head())
+
+
